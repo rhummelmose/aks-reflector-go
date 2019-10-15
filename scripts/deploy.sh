8f4372d03d2ce13684ca8e0f59346be3945be9b6
Rasmus Hummelmose <rasmus@hummelmose.dk>
2019-10-15 19:13:30 +0200

    Use Azure CLI task instead of Kubectl

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# Authenticate with the cluster 
+az aks get-credentials --resource-group $ARGO_RESOURCE_GROUP --name $ARGO_AKS_CLUSTER_NAME --admin
 
+# Kubectl
+kubectl apply -f kubernetes/configmaps.yml
+kubectl apply -f kubernetes/secrets.yml
+kubectl apply -f kubernetes/deployments.yml
+kubectl apply -f kubernetes/services.yml
+kubectl apply -f kubernetes/ingresses.yml
