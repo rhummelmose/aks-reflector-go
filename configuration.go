7350a7721a1689c0e747c6d68ae0cc2fa633299d
Rasmus Hummelmose <rasmus@hummelmose.dk>
2019-10-16 07:07:42 +0200

    Correction in env names

--- configuration.go
+++ configuration.go
@@ -19,12 +19,12 @@ type Configuration struct {
 // NewConfigurationFromEnv returns a configuration object built by retrieving values from env
 func NewConfigurationFromEnv() Configuration {
 	var configuration Configuration
-	configuration.ServicePrincipalUsername = os.Getenv("AWGO_SP_USERNAME")
-	configuration.ServicePrincipalPassword = os.Getenv("AWGO_SP_PASSWORD")
-	configuration.AzureADTenant = os.Getenv("AWGO_AZURE_AD_TENANT")
-	configuration.ResourceGroup = os.Getenv("AWGO_RESOURCE_GROUP")
-	configuration.AKSClusterName = os.Getenv("AWGO_AKS_CLUSTER_NAME")
-	listeningPort, listeningPortErr := strconv.Atoi(os.Getenv("AWGO_LISTENING_PORT"))
+	configuration.ServicePrincipalUsername = os.Getenv("ARGO_SP_USERNAME")
+	configuration.ServicePrincipalPassword = os.Getenv("ARGO_SP_PASSWORD")
+	configuration.AzureADTenant = os.Getenv("ARGO_AZURE_AD_TENANT")
+	configuration.ResourceGroup = os.Getenv("ARGO_RESOURCE_GROUP")
+	configuration.AKSClusterName = os.Getenv("ARGO_AKS_CLUSTER_NAME")
+	listeningPort, listeningPortErr := strconv.Atoi(os.Getenv("ARGO_LISTENING_PORT"))
 	if listeningPortErr != nil {
 		log.Fatal(listeningPortErr)
 	}
